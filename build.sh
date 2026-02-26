6e487e34c11f214495cd528a3dcac6cd29d53f85
MD SAMEER <sdukesameer@gmail.com>
2026-02-26 16:58:12 +0000

    Add .python-version file and update build script for Python 3.11 compatibility

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+# Build script for Render.com
 set -e
-pip install lbry-libtorrent flask gunicorn
+
+# python-libtorrent has prebuilt wheels for Python 3.11 on Linux x86_64
+pip install python-libtorrent flask gunicorn
